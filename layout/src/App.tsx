1e6e9bccd775dbf09289122bd52847d15fe6c3a5
nagai hiroto <nagai@cyberbeing.co.jp>
2022-01-10 12:56:14 +0900

    first commit

---
+++ layout/src/App.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+function App() {
+  return (
+    <_Frame>
+    </_Frame>
+  );
+}
+
+export default App;
+
+const _Frame = styled.div`
+    display: inline-block;
+    width: 100px;
+    height: 100px;
+    background-color: #d60000
+`;
+
